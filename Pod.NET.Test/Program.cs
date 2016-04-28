26b5672d24c610cd44de9353ad9149b4cc54bc1f
Ray Koopa <raykoopa@outlook.com>
2016-04-28 20:29:23 +0200

    Initial commit. Basic decryption support (needs testing), BL4 header and event loading.

---
+++ Pod.NET.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Pod.NET.Test
+{
+    using PodNET.BL4;
+
+    /// <summary>
+    /// Main class of the program containing the application entry point.
+    /// </summary>
+    internal class Program
+    {
+        // ---- METHODS (PRIVATE) --------------------------------------------------------------------------------------
+
+        private static void Main(string[] args)
+        {
+            //PodBinaryDataFile pbdf;
+            BL4Track beltane = new BL4Track(@"C:\Games\Pod\DATA\BINARY\CIRCUITS\BELTANE.BL4");
+            //pbdf = new PodBinaryDataFile(@"C:\Games\Pod\DATA\BINARY\VOITURES\SCORP.BV4");
+        }
+    }
+}
---
+++ Pod.NET/PodCryptoTransformConfig.cs
@@ -0,0 +1,61 @@
+﻿namespace PodNET
+{
+    using System;
+
+    /// <summary>
+    /// Represents the initial configuration of a encryption and decryption progress of Pod binary data files.
+    /// </summary>
+    internal class PodCryptoTransformConfig
+    {
+        // ---- CONSTRUCTORS & DESTRUCTOR ------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PodCryptoTransformConfig"/> class.
+        /// </summary>
+        internal PodCryptoTransformConfig()
+        {
+        }
+
+        // ---- PROPERTIES ---------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the cryptor currently encrypts (true) or decrypts (false).
+        /// </summary>
+        internal bool Encrypt
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the size of the loaded binary file. It is used to determine the coding key for the first data
+        /// block.
+        /// </summary>
+        internal uint FileSize
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the coder key which is normally retrieved by XORing the file size specified in the header with
+        /// the given <see cref="FileSize"/>.
+        /// </summary>
+        internal uint CoderKey
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the blocksize of file blocks after which checksums occur. This is normally determined by
+        /// checking if possible checksums are at a position which would result in a blocksize of which a multiple is
+        /// the <see cref="FileSize"/>.
+        /// </summary>
+        internal uint BlockSize
+        {
+            get;
+            set;
+        }
+    }
+}
